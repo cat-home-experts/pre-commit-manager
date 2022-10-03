7d45c8689716c56c88c372242198470848f57b96
Ryan Deamer <ryan.deamer@checkatrade.com>
2022-10-03 17:54:14 +0100

    add provider cleanup

---
+++ pre_commit_python_hooks/remove_provider.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+import argparse
+import re
+import os
+
+
+PASS = 0
+FAIL = 1
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    ISSUE = ""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*", help="Filenames to fix")
+    args = parser.parse_args(argv)
+    # dir(args)
+
+    for arg in args.filenames:
+        folder = re.search("(.*/).*.tf", arg)
+
+        providerLocation = folder.group(1) + "provider.tf"
+        os.remove(providerLocation)
+
+    if ISSUE != "":
+        return FAIL
+    else:
+        return PASS
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
