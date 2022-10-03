39b466c6353c748fa803017ebdda360fc06bbab6
Ryan Deamer <ryan.deamer@checkatrade.com>
2022-10-03 16:49:52 +0100

    add build_provider for terraform folder location

---
+++ pre_commit_python_hooks/build_provider.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+import argparse
+import re
+
+PASS = 0
+FAIL = 1
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    ISSUE = ""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*", help="Filenames to fix")
+    args = parser.parse_args(argv)
+    # dir(args)
+
+    for arg in args.filenames:
+        folder = re.search("(.*/).*.tf", arg)
+        print(folder.group(1))
+        data = ""
+        hcl = "../../terragrunt.hcl"
+        with open(hcl, "r+") as file:
+            # lines = file.readlines()
+            inRecordingMode = False
+            for line in file:
+                # print(line)
+                if not inRecordingMode:
+                    if line.startswith("    terraform {"):
+
+                        data += line
+
+                        inRecordingMode = True
+                elif line.startswith("  EOF"):
+                    break
+                    print(line)
+                    inRecordingMode = False
+                else:
+                    data += line
+
+        print(data)
+        providerLocation = folder.group(1) + "provider.tf"
+        with open(providerLocation, "w+") as provider:
+            provider.write(data)
+        provider.close()
+
+    if ISSUE != "":
+        return FAIL
+    else:
+        return PASS
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
