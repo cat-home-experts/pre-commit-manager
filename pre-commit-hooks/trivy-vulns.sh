8bda2d7d37a8ab05052f384ce8386b864b40f96c
Ryan Deamer <ryan.deamer@checkatrade.com>
2022-11-14 11:14:30 +0000

    add echo output

--- pre-commit-hooks/trivy-vulns.sh
+++ pre-commit-hooks/trivy-vulns.sh
@@ -6,4 +6,4 @@ set -e -u -o pipefail
 
 
 
-trivy fs --severity high,CRITICAL -q --exit-code=1  --ignore-unfixed --security-checks vuln .
+echo "$(trivy fs --severity high,CRITICAL -q --exit-code=1  --ignore-unfixed --security-checks vuln .)"
